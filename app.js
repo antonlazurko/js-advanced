135dcac7483269b25436d7d0dc655c8a79b5d3d3
Anton Lazurko <antla@ciklum.com>
2024-10-21 22:55:32 +0300

    feat: number separator

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 'use strict'
-console.log(15 / 2);// 7.5
-console.log(15 % 2);// 1
-console.log(14 % 2);// 0
+console.log(3_5000_000);//350000000
+console.log(10_20);//
 
-function isEven(n) {
-    if (n % 2 === 0) return true
-    return false
-}
-function isOdd(n) {
-    if (n % 2 === 1) return true
-    return false
-}
+console.log(Number('10_20'));//NuN
+console.log(Number.parseInt('10_20'));//10
 
-console.log(isEven(15));// false
-console.log(isOdd(15));// true
+
 
