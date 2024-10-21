bc8dc2001c4eb4a1463c3174827e1ee19d53fe7b
Anton Lazurko <antla@ciklum.com>
2024-10-21 14:15:00 +0300

    Map creation

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 'use strict'
-const weatherMap = new Map()
-const arrayInMap = [1,2,3]
-weatherMap
-    .set('London', 10)
-    .set('Kyiv', 5)
-    .set('Vinnytsia', 11)
-    .set('Vinnytsia', 11)
-    .set('Rome', 11)
-    .set([1,2,3], 11)
-    .set({a: 1}, 11)
-    .set(arrayInMap, 11)
 
-console.log(weatherMap.get('Kyiv'))
-console.log(weatherMap.get('test'))
-console.log(weatherMap.get('test'))
-console.log(weatherMap.get([1,2,3]))// search only by link
-console.log(weatherMap.has({a: 1}))// search only by link
-console.log(weatherMap.get(arrayInMap))
-console.log(weatherMap.has(arrayInMap))
-console.log(weatherMap.has('Kyiv'))
-console.log(weatherMap.size);
+const weatherMap = new Map([
+    ['London', 10],
+    ['Rome', 11],
+    ['Mexico', 12]
+])
+
 console.log(weatherMap);
-weatherMap.delete('Vinnytsia')
-console.log(weatherMap);
-weatherMap.clear()
-console.log(weatherMap);
+
+const obj = {
+    london: 10,
+    rome: 15,
+    mexico: 13,
+}
+
+console.log(Object.entries(obj));
+
+const weatherMapFromObj = new Map(Object.entries(obj))
+console.log(weatherMapFromObj);
+
 
