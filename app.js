97b052ce82d7847b7332ae062a5cca0a1ac1edc7
Anton Lazurko <antla@ciklum.com>
2024-10-21 22:46:14 +0300

    feat: exercise function random number

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 'use strict'
-
-console.log(Math.round(1.6))//2
-console.log(Math.ceil(1.6))//2;
-console.log(Math.floor(1.6))//1;
-console.log(Math.trunc(1.6))//1;
-
-console.log(1.4999.toFixed(2))//1.50: string;
-console.log(Number((1.4999).toFixed(1)));//1.5: number;
-
-
-
-
-
-
-
-
-
-
-
-
-
+//function random number generator
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRandomInt(1, 20));
