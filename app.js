b9a635c8f88627211af821644d0e3d182cf9fa9f
Anton Lazurko <antla@ciklum.com>
2024-10-21 22:50:09 +0300

    feat: modulus operator

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict'
-//function random number generator
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(15 / 2);// 7.5
+console.log(15 % 2);// 1
+console.log(14 % 2);// 0
+
+function isEven(n) {
+    if (n % 2 === 0) return true
+    return false
 }
-console.log(getRandomInt(1, 20));
+function isOdd(n) {
+    if (n % 2 === 1) return true
+    return false
+}
+
+console.log(isEven(15));// false
+console.log(isOdd(15));// true
+
