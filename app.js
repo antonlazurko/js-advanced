62b84104b6ec62063c6c8d3034321ceab9efd84c
Anton Lazurko <antla@ciklum.com>
2024-10-21 12:29:07 +0300

    feat: work with Set

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 'use strict'
-console.log('Test');
+const array = ['Test', 'Test1', 'Test2', 'Test', 1, false, null]
+const set = new Set(array)
+console.log(set);
+console.log(set.size);
+set.has('Test')
+set.add('Test3')
+set.add(undefined)
+set.delete(null)
+console.log(set.has(null))
+console.log(set);
+
+for (const el of set) {
+    console.log(el);
+}
+
+console.log([...set]);
+
+const setObj = new Set([{a: 1}, {b: 2}, {b: 2}])
+console.log(setObj);
+
+const setStr = new Set('abcd')
+console.log(setStr);
+// const setFromObj = new Set({a: 1}) //Error not iterable
+// const setFromBoolean = new Set(false) //Error not iterable
