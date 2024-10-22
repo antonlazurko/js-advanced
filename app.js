eb288daeaae59f3b137c639ae85178f299c7d663
Anton Lazurko <antla@ciklum.com>
2024-10-22 11:57:26 +0300

    feat: date creation

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 'use strict'
-console.log('Test');
+
+const now = new Date()
+console.log(`Hello World, it is ${now}.`)
+console.log(new Date('2020-01-01').toUTCString());
+console.log(new Date('2020-01-01'));
+console.log(new Date('Wed Jan 01 2020 02:00:00 GMT+0200'));
+
+console.log(new Date(2022, 1, 29))
+console.log(new Date(2 * 24 * 60 * 60 * 1000));// 2 days
+
+console.log('----------------------------------');
+
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+
+console.log('----------------------------------');
+
+console.log(new Date(now.setFullYear(2025)));
+
+
+
