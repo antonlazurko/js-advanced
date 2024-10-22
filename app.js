308eb28d19aa02a4deb986e6d15f5030ddafcc14
Anton Lazurko <antla@ciklum.com>
2024-10-22 11:41:24 +0300

    feat: exercise currency converter

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 'use strict'
-const options = {
-    style: 'currency',
-    currency: 'UAH',
-    minimumFractionDigits: 2,
-    // useGrouping: false
-}
-const usOptions = {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 1
-}
 
+function getConvertedCurrencyString(sum, currencyFrom, currencyTo) {
+    const allCurrenciesMultiplier =  { 'UAH': 1/41.43, 'USD': 1, 'EUR': 1.08 }
 
-console.log(new Intl.NumberFormat('ua-UA', options).format(1234.56));// 12 345,56 грн
-console.log(new Intl.NumberFormat('us-US', usOptions).format(100022200));   //$100,022,200.00
-console.log(new Intl.NumberFormat('us-US', {style: 'decimal'}).format(100022200));  //100,022,200
-console.log(new Intl.NumberFormat('us-US', {style: 'percent'}).format(0.56));   //56%
-console.log(new Intl.NumberFormat('us-US', {style: 'unit', unit: 'celsius'}).format(100));//100 °C
+    const formats = {
+        'USD': 'en-US',
+        'UAH': 'uk-UA',
+        'EUR': 'de-DE'
+    };
+
+    if (!allCurrenciesMultiplier[currencyFrom] || !allCurrenciesMultiplier[currencyTo]) {
+        return null;
+    }
+    const multiplier = allCurrenciesMultiplier[currencyFrom] / allCurrenciesMultiplier[currencyTo];
+    const formattedSum = new Intl.NumberFormat(formats[currencyTo], {
+        style: 'currency',
+        currency: currencyTo,
+        useGrouping: false
+    }).format(sum * multiplier);
+
+    return formattedSum;
+}
+console.log(getConvertedCurrencyString(1000, 'USD', 'UAH'))
+console.log(getConvertedCurrencyString(1000, 'UAH', 'USD'))
+console.log(getConvertedCurrencyString(1000, 'USD', 'EUR'))
+console.log(getConvertedCurrencyString(1000, 'UAH', 'EUR'))
+console.log(getConvertedCurrencyString(1000, 'DAK', 'EUR'))
+console.log(getConvertedCurrencyString(1000, 'EUR', 'UAH'))
