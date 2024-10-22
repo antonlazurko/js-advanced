672ddbee7733b36800c0862452508f3e578bde09
Anton Lazurko <antla@ciklum.com>
2024-10-22 12:07:02 +0300

    feat: dates comparison

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 'use strict'
 
 const date1 = new Date(2024, 10, 15)
+const sameDateTheDate1 = new Date(2024, 10, 15)
 const date2 = new Date(2024, 11, 15)
 
-console.log(date2 - date1);
-
-function getDaysBetweenDates(dateFirst, dateSecond) {
-    return (dateSecond - dateFirst) / (1000 * 60 * 60 * 24);
-}
-
-console.log(getDaysBetweenDates(date1, date2));
-
+console.log(date1 > date2);
+console.log(date1 === sameDateTheDate1);//false
+console.log(date1.getTime() === sameDateTheDate1.getTime());
