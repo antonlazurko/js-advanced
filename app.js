c3548e9a781ee7c35e8ba5a14e49575ee91a40ea
Anton Lazurko <antla@ciklum.com>
2024-10-22 10:12:35 +0300

    feat: bigInt

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 'use strict'
-console.log(3_5000_000);//350000000
-console.log(10_20);//
+const max = Number.MAX_SAFE_INTEGER
+const min = Number.MIN_SAFE_INTEGER
 
-console.log(Number('10_20'));//NuN
-console.log(Number.parseInt('10_20'));//10
+const bigNumber = 123436234234034534534592354234
+console.log(typeof 123436234234034534534592354234n);
+console.log(bigNumber === 123436234234034534534592354234n); // false
+console.log(bigNumber == 123436234234034534534592354234n); // true
+console.log(BigInt(bigNumber));
+console.log(BigInt(bigNumber) + 10);// Error
+console.log(BigInt(bigNumber)  * BigInt(bigNumber));// 123436234234034541096924610560n
+
+console.log(10n / 3n);  // 3n
 
 
 
