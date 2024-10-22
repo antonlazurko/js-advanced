b01443bdea3c231e2ae0f795b5f5f3c9d063a573
Anton Lazurko <antla@ciklum.com>
2024-10-22 10:22:03 +0300

    feat: number internalization

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 'use strict'
-const max = Number.MAX_SAFE_INTEGER
-const min = Number.MIN_SAFE_INTEGER
-
-const bigNumber = 123436234234034534534592354234
-console.log(typeof 123436234234034534534592354234n);
-console.log(bigNumber === 123436234234034534534592354234n); // false
-console.log(bigNumber == 123436234234034534534592354234n); // true
-console.log(BigInt(bigNumber));
-console.log(BigInt(bigNumber) + 10);// Error
-console.log(BigInt(bigNumber)  * BigInt(bigNumber));// 123436234234034541096924610560n
-
-console.log(10n / 3n);  // 3n
-
+const options = {
+    style: 'currency',
+    currency: 'UAH',
+    minimumFractionDigits: 2,
+    // useGrouping: false
+}
+const usOptions = {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 1
+}
 
 
+console.log(new Intl.NumberFormat('ua-UA', options).format(1234.56));// 12 345,56 грн
+console.log(new Intl.NumberFormat('us-US', usOptions).format(100022200));   //$100,022,200.00
+console.log(new Intl.NumberFormat('us-US', {style: 'decimal'}).format(100022200));  //100,022,200
+console.log(new Intl.NumberFormat('us-US', {style: 'percent'}).format(0.56));   //56%
+console.log(new Intl.NumberFormat('us-US', {style: 'unit', unit: 'celsius'}).format(100));//100 °C
