89c7329e20e8c04ea556cdc6afabd2c843ee52e8
Anton Lazurko <antla@ciklum.com>
2024-10-23 23:26:16 +0300

    feat:  prototype chaining

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 'use strict'
 
-const product = {
-    id: 1,
-    name: 'laptop',
-    price: 1000
+const Book = function (title, author) {
+    this.title = title
+    this.author = author
 }
-
-const Cart = function() {
-    this.products = []
-    this.count = 0
-}
-Cart.prototype.addProduct = function(product) {
-    if (this.products.find(p => p.product.id === product.id)) {
-        return
-    }
-    this.products.push({product})
-    this.count++
-}
-Cart.prototype.removeProduct = function(product) {
-    const index = this.products.indexOf(product)
-    if (index > -1) {
-        this.products.splice(index, 1)
-        this.count--
-    }
-}
-const cart = new Cart(product)
-cart.addProduct(product)
-cart.removeProduct(product)
-cart.addProduct({...product, id: 2})
-console.log(cart)
-
+const book1 = new Book('title1', 'author1')
+Book.prototype.isRead = false
+console.log(book1)
+console.log(book1.hasOwnProperty('title'))
+console.log(book1.hasOwnProperty('isRead'))//false
+console.log(Book.prototype.__proto__)
