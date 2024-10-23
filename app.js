a9ead957dfcb555373bd73e6cfb06072a5f55bc0
Anton Lazurko <antla@ciklum.com>
2024-10-23 16:47:11 +0300

    feat: timers creation

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict'
-console.log('Test');
+
+const timer = setTimeout((...val) => {
+    val.forEach((value) => {
+        console.log(value);
+    })
+}, 1000, 'Test', 'Test1', 'Test2', 'Test3');
+console.log(timer);
+// clearTimeout(timer);
+console.log('test');
+
