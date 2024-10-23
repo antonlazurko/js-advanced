54e9044a6ee7ac27bc13b88a7f4e6b3efae255e9
Anton Lazurko <antla@ciklum.com>
2024-10-23 23:01:44 +0300

    feat: prototype

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 'use strict'
 
-const User = function (email, password){
-    this.email = email
-    this.password = password
+const Book = function (title, author){
+    this.title = title
+    this.author = author
+    this.isRead = false
+    // this.read = function () {
+    //     this.isRead = true
+    // }
 }
-const user1 = new User('abc@gmail.com', '123')
-const user2 = new User('abc2@gmail.com', '1234')
-console.log(user1);
-console.log(user1 instanceof User);
+Book.prototype.read = function () {
+    this.isRead = true
+}
+Book.prototype.cover = 'paper'
+const book = new Book('book', 'Joe Doe')
+const book2 = new Book('book2', 'Joe Doe jr')
+console.log(book);
+console.log(book.hasOwnProperty('cover'));
+book.cover = 'paper2'
+console.log(book.hasOwnProperty('cover'));
+console.log(book.__proto__);
+console.log(book.__proto__ === Book.prototype);
+book.read()
+console.log(book instanceof Book);
+console.log(Book.prototype.isPrototypeOf(Book));
+console.log(Book.prototype.isPrototypeOf(book));
+Array.prototype.first = function () {
+    return this[0]
+}
 
