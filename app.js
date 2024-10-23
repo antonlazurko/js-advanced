df61e17558807fae023461d5f4406f24764d27c6
Anton Lazurko <antla@ciklum.com>
2024-10-23 22:44:19 +0300

    feat: operator new

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 'use strict'
-console.log('Test');
+
+const User = function (email, password){
+    this.email = email
+    this.password = password
+}
+const user1 = new User('abc@gmail.com', '123')
+const user2 = new User('abc2@gmail.com', '1234')
+console.log(user1);
+console.log(user1 instanceof User);
+
