2420ececcf2bfe71b6a3ede7ed1bf852c41edd8a
Anton Lazurko <antla@ciklum.com>
2024-10-24 10:57:02 +0300

    feat: getter and setters

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 'use strict'
 
-class Book {
-    isRead = false
-    constructor(title, author){
-        this.title = title
-        this.author = author
-    }
-    read(){
-        this.isRead = true
+const task = {
+    title: 'My task',
+    dueTo: new Date('2023/01/01'),
+    get overdue() {
+        return this.dueTo < new Date();
+    },
+    set overdue(value) {
+        if (!value) {
+            this.dueTo = new Date();
+        }
     }
 }
-const book = new Book('title', 'author')
+console.log(task.overdue);
+task.overdue = false
+console.log(task.overdue);
 
-console.log(book instanceof Book);
+class Task {
+    constructor(title, dueTo) {
+        this.title = title;
+        this.dueTo = dueTo;
+    }
+    get overdue() {
+        return this.dueTo < new Date();
+    }
+    set dueTo(date) {
+        if (date < new Date()) {
+            return
+        }
+        this._dueTo = date
+    }
+}
+const newTask = new Task('My task', new Date('2024/01/11'));
+newTask.dueTo = new Date('2023/01/02');
+console.log(newTask._dueTo);
