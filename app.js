7d9ade30d1b8ed5bc7f8b31982011e2b04c4d7ec
Anton Lazurko <antla@ciklum.com>
2024-10-24 10:46:30 +0300

    feat: class init

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict'
-console.log('Test');
+
+class Book {
+    isRead = false
+    constructor(title, author){
+        this.title = title
+        this.author = author
+    }
+    read(){
+        this.isRead = true
+    }
+}
+const book = new Book('title', 'author')
+
+console.log(book instanceof Book);
