ec4f2e8c02806848add8dc4fb4b044cd58d561d9
Anton Lazurko <antla@ciklum.com>
2024-10-24 11:49:49 +0300

    feat : Object.create

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
 'use strict'
 
-class User {
-    #login
-    #_password
-    constructor(login, password) {
-        this.#login = login
-        this.#_password = this.#encryptPassword(password)
-    }
-    get login() {
-        return this.#login
-    }
-    set #password (password){
-        this.#_password = this.#encryptPassword(password)
-    }
-    get #password(){
-        return this.#encryptPassword(this.#_password)
-    }
-    #encryptPassword(password) {
-        return password.split('').reverse().join('')
-    }
-    #passwordValidation(oldPassword){
-        console.log('correct password');
-
-        return this.#password = oldPassword
-    }
-    changePassword(oldPassword, newPassword){
-        if (this.#passwordValidation(oldPassword)) {
-
-            this.#_password = this.#encryptPassword(newPassword)
-            console.log('password changed');
-        }
-
+const User = {
+    init(email, password){
+        this.email = email;
+        this.password = password;
+    },
+    log(){
+        console.log('log');
     }
 }
+const user = Object.create(User); // has User as prototype
+const admin = {...User}; // copy of User
+console.log(user.__proto__ === User);
+user.log();
+user.init('a', 'b');
+admin.init('c', 'd');
 
-
-const user = new User('t@t.com', '1234567')
 console.log(user);
-
-user.changePassword('1234567', '12345678')
-console.log(user);
-console.log(user.login);
-
+console.log(admin);
