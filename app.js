ba1312018a653d1b9f319668b2faaf9bc6ab18b0
Anton Lazurko <antla@ciklum.com>
2024-10-24 11:03:14 +0300

    feat: static methods and properties

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 'use strict'
 
-const task = {
-    title: 'My task',
-    dueTo: new Date('2023/01/01'),
-    get overdue() {
-        return this.dueTo < new Date();
-    },
-    set overdue(value) {
-        if (!value) {
-            this.dueTo = new Date();
-        }
+class Test {
+    static a = 1
+    static hello() {
+        console.log('hello')
     }
-}
-console.log(task.overdue);
-task.overdue = false
-console.log(task.overdue);
+    static {//static block
+        let b = 5
+        this.a = b
+    }
 
-class Task {
-    constructor(title, dueTo) {
-        this.title = title;
-        this.dueTo = dueTo;
-    }
-    get overdue() {
-        return this.dueTo < new Date();
-    }
-    set dueTo(date) {
-        if (date < new Date()) {
-            return
-        }
-        this._dueTo = date
-    }
 }
-const newTask = new Task('My task', new Date('2024/01/11'));
-newTask.dueTo = new Date('2023/01/02');
-console.log(newTask._dueTo);
+Test.hello()
+console.log(Test.a);
+
+
+// const Test2 = function() {
+// }
+// Test2.hello = function() {
+//     console.log('hello')
+// }
+// Test2.hello()
