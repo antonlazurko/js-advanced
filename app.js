c8760ea4724dff72bfd6769c883ebe42b4065414
Anton Lazurko <antla@ciklum.com>
2024-10-25 11:11:59 +0300

    feat: abstraction and encapsulation

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 'use strict'
-console.log('Test');
+
+class Film {
+    #name
+    #author
+    rating
+    #length
+    constructor(name, author, length){
+        this.#name = name
+        this.#author = author
+        this.#length = length
+    }
+    get name (){
+        return this.#name
+    }
+    get author (){
+        return this.#author
+    }
+    get length (){
+        return this.#length
+    }
+}
+const film = new Film('name', 'author', 100)
