6804d0431b32cf8d7ce36c11ea1f5217d08d3eda
Anton Lazurko <antla@ciklum.com>
2024-10-25 11:40:44 +0300

    feat: inheritance in ES6 and override

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 'use strict'
 
-const Book = function (title, author) {
-    this.title = title
-    this.author = author
-}
+// const Book = function (title, author) {
+//     this.title = title
+//     this.author = author
+// }
 
-Book.prototype.getSummary = function () {
-    console.log(`${this.title} was written by ${this.author}`);
+// Book.prototype.getSummary = function () {
+//     console.log(`${this.title} was written by ${this.author}`);
 
-}
+// }
 
-const AudioBook = function (title, author, length) {
-    Book.call(this, title, author)
-    this.length = length
+// const AudioBook = function (title, author, length) {
+//     Book.call(this, title, author)
+//     this.length = length
+// }
+// AudioBook.prototype = Object.create(Book.prototype)
+// AudioBook.prototype.constructor = AudioBook
+// AudioBook.prototype.log = function () {
+//     console.log(`${this.title} is ${this.length} minutes long`);
+// }
+// const book = new AudioBook('The Hobbit', 'J.R.R. Tolkien', 295)
+// book.log()
+
+// book.getSummary()
+// console.log(book);
+// console.log(book instanceof AudioBook);
+// console.log(book instanceof Book);
+
+class Book {
+    constructor(title, author) {
+        this.title = title
+        this.author = author
+    }
+    getSummary() {
+        console.log(`${this.title} was written by ${this.author}`);
+    }
 }
-AudioBook.prototype = Object.create(Book.prototype)
-AudioBook.prototype.constructor = AudioBook
-AudioBook.prototype.log = function () {
-    console.log(`${this.title} is ${this.length} minutes long`);
+class AudioBook extends Book {
+    constructor(title, author, length) {
+        super(title, author)
+        this.length = length
+    }
+    //override
+    getSummary() {
+
+        console.log(`${this.title} is ${this.length} minutes long`);
+    }
+    log() {
+        const hours = Math.floor(this.length / 60);
+        const minutes = this.length % 60;
+        console.log(`${this.title} is ${hours} hours and ${minutes} minutes long`);
+    }
 }
 const book = new AudioBook('The Hobbit', 'J.R.R. Tolkien', 295)
-book.log()
-
 book.getSummary()
 console.log(book);
+book.log()
 console.log(book instanceof AudioBook);
 console.log(book instanceof Book);
