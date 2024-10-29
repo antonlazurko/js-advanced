0576336e442bdf0288238cdd29f406d3951c5288
Anton Lazurko <antla@ciklum.com>
2024-10-29 10:02:57 +0200

    feat: dependency injection principle

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 'use strict'
 
-class Weapon {
-    cost
-    dealDamage(){}
-}
+class DB {
+    save(items) {
+        console.log('Saving to DB' + items);
 
-class Rifle extends Weapon {
-    shoot() {
-        this.dealDamage()
     }
 }
 
-class Sword extends Weapon {
-    strike() {
-        this.dealDamage()
+class MongoDB extends DB {
+    save(items) {
+        console.log('Saving to MongoDB' + items);
     }
 }
+
+class ToDoList {
+    items = [1, 2, 3]
+    db
+    constructor(db) {
+        this.db = db
+    }
+    saveToDB() {
+        this.db.save(this.items)
+    }
+}
+
+const list1 = new ToDoList(new MongoDB())
+const list2 = new ToDoList(new DB())
+list1.saveToDB()
+list2.saveToDB()
