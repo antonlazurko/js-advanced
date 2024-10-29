103f9afb99e87f907fb984fd8ce17feb9546c164
Anton Lazurko <antla@ciklum.com>
2024-10-29 09:41:10 +0200

    feat: single responsibility

--- app.js
+++ app.js
@@ -1,56 +1,22 @@
 'use strict'
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array.map(num => num * 2).filter(num => num > 10).find(num => num === 12)
-
-class Wallet {
-    balance = 0
-    add(amount) {
-        this.balance += amount
-        return this
+class Character {
+    #inventory = []
+    #health = 10
+    pickItem(item) {
+        this.#inventory.push(item)
     }
-    remove(amount) {
-        this.balance -= amount
-        return this
+    receiveDamage(damage) {
+        this.#health -= damage
     }
 }
-const wallet = new Wallet()
-wallet
-    .add(10)
-    .remove(5)
-    .add(15)
-    .remove(10)
-    .add(20)
-    .remove(5)
-console.log(wallet.balance)
 
-class Builder {
-    house = {}
-    setDoor(door) {
-        this.house.door = door
-        return this
+class LocalStorage {
+    save(item) {
+        localStorage.setItem('item', JSON.stringify(item))
     }
-    setWindow(window) {
-        this.house.window = window
-        return this
-    }
-    setRoof(roof) {
-        this.house.roof = roof
-        return this
-    }
-    setStage(stage) {
-        this.house.stage = stage
-        this.setWindow(this.house.window  * stage)
-        return this
-    }
-    build() {
-        return this.house
+    load() {
+        const item = JSON.parse(localStorage.getItem('item'))
+        return item
     }
 }
-const house = new Builder()
-    .setDoor(1)
-    .setWindow(2)
-    .setRoof(3)
-    .setStage(4)
-    .build()
-console.log(JSON.stringify(house))
