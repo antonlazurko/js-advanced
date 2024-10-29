5f3e5d5f1a81cad7265527180ffa99667d236904
Anton Lazurko <antla@ciklum.com>
2024-10-29 14:38:33 +0200

    feat: callback hell

--- app.js
+++ app.js
@@ -3,16 +3,26 @@
 const baseUrl = 'https://dummyjson.com/'
 const path = 'products/'
 
-function req(id) {
+function req(id) {console.log(id);
+
     const request = new XMLHttpRequest()
-    request.open('GET', baseUrl + path + (id))
+    request.open('GET', baseUrl + path + id)
     request.send()
 
     request.addEventListener('load', () => {
         const {products =  [], limit} = JSON.parse(request.response)
-        const price = products.reduce((acc, product) => acc + product.price, 0)
-        console.log(price / limit)
-        return price
+            const price = products.reduce((acc, product) => acc + product.price, 0)
+            console.log(price / limit)
+
+            const requestProduct = new XMLHttpRequest()
+            requestProduct.open('GET', baseUrl + path + products[0].id)
+            requestProduct.send()
+
+            requestProduct.addEventListener('load', () => {
+            const product = JSON.parse(requestProduct.response)
+            console.log(product)
+
+    })
     })
 }
 req('')
