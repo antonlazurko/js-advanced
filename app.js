841300543d39b534316c3946da61ae7b1192fd2e
Anton Lazurko <antla@ciklum.com>
2024-10-29 09:52:21 +0200

    feat Liskov substitution principle

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 'use strict'
 
-class Treasure {
-    value = 0
-}
-
-class Coin extends Treasure {
-    value = 1
-}
-
-class Diamond extends Treasure {
-    value = 10
-}
-class Crystal extends Treasure {
-    value = 100
-}
-
-class Inventory {
-    #score
-    pick(treasure) {
-        this.#score += treasure.value
-        console.log(`Score: ${this.#score}`)
+class User {
+    #role = 'user'
+    getRole() {
+        return this.#role
     }
 }
+
+class Admin extends User {
+    #role = ['user', 'admin']
+    getRole() {
+        return this.#role.join(', ')
+    }
+}
+
+function logRole(user) {
+    console.log('User role: ', user.getRole().toUpperCase())
+
+}
+
+console.log(logRole(new User()));
+console.log(logRole(new Admin()));
