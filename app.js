f4d4a5709d647b4e34949f1ebfe730ae598f9d79
Anton Lazurko <antla@ciklum.com>
2024-10-30 10:14:40 +0200

    feat: custom error handling

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
 'use strict'
 
 const baseUrl = 'https://dummyjson.com/'
-const path = 'products/categories'
-const app = document.getElementById('app')
-
-function createAndAppendSelect(entityArray) {
-    const select = document.createElement('select')
-    entityArray.forEach(({name, url}) => {
-        const option = document.createElement('option')
-        option.textContent = name
-        option.value = url
-        select.appendChild(option)
-    })
-    app.appendChild(select)
-    document.querySelector('select').addEventListener('change', (event) => {
-        console.log(event.target.value)
-        const message = `Selected category: ${event.target.value}`;
-
-        const h1 = document.createElement('h1')
-        h1.textContent = message
-        h1.style.color = 'blue'
-        app.appendChild(h1)
-    })
-}
+const path = 'productsr'
 
 fetch(baseUrl + path)
     .then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText)
+        }
         console.log(response);
         return response.json()
     })
-    .then((categories) => {
-        console.log(categories)
-        createAndAppendSelect(categories)
+    .then((products) => {
+        console.log(products)
+        return fetch(baseUrl + path + '/' + products.products[0].id)
     })
     .catch(({message}) => {
-        const h1 = document.createElement('h1')
-        h1.textContent = message
-        h1.style.color = 'red'
-        app.appendChild(h1)
+        console.log(message)
+        const app = document.getElementById('app')
+        app.innerHTML = message
+        app.style.color = 'red'
     })
     .finally(() => console.log('finally'))
