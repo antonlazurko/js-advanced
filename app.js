82bf4ed7e7aebce9e0b0b7922b332f03de165c2f
Anton Lazurko <antla@ciklum.com>
2024-10-30 09:49:38 +0200

    feat: reject promises

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 'use strict'
 
-const baseUrl = 'https://dummyjson.com/'
+const baseUrl = 'https://dummyjsons.com/'
 const path = 'products/'
 
 fetch(baseUrl + path)
-    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        return response.json()
+    },
+    // error => console.log(error)
+    )
     .then(({products}) => {
         console.log(products)
         return fetch(baseUrl + path + products[0].id)
-    })
+    },
+    // error => console.log(error)
+)
     .then(response => response.json())
     .then(product => {
         console.log(product)
-    })
+    }).catch(error => console.log(error), )
