7187c97de6a4d305371f01a00d5f183b21738ddd
Anton Lazurko <antla@ciklum.com>
2024-10-30 10:06:36 +0200

    feat: exercise select from response categories

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 'use strict'
 
 const baseUrl = 'https://dummyjson.com/'
-const path = 'products/'
+const path = 'products/categories'
+const app = document.getElementById('app')
+
+function createAndAppendSelect(entityArray) {
+    const select = document.createElement('select')
+    entityArray.forEach(({name, url}) => {
+        const option = document.createElement('option')
+        option.textContent = name
+        option.value = url
+        select.appendChild(option)
+    })
+    app.appendChild(select)
+    document.querySelector('select').addEventListener('change', (event) => {
+        console.log(event.target.value)
+        const message = `Selected category: ${event.target.value}`;
+
+        const h1 = document.createElement('h1')
+        h1.textContent = message
+        h1.style.color = 'blue'
+        app.appendChild(h1)
+    })
+}
 
 fetch(baseUrl + path)
     .then(response => {
         console.log(response);
         return response.json()
-    },
-    // error => console.log(error)
-    )
-    .then(({products}) => {
-        console.log(products)
-        return fetch(baseUrl + path + products[0].id)
-    },
-    // error => console.log(error)
-    )
-    .then(response => response.json())
-    .then(product => {
-        console.log(product)
     })
-    .catch(error => console.log(error))
+    .then((categories) => {
+        console.log(categories)
+        createAndAppendSelect(categories)
+    })
+    .catch(({message}) => {
+        const h1 = document.createElement('h1')
+        h1.textContent = message
+        h1.style.color = 'red'
+        app.appendChild(h1)
+    })
     .finally(() => console.log('finally'))
