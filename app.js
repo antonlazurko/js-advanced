fe05fa3eb842ccf63ea3c697f03644701082746e
Anton Lazurko <antla@ciklum.com>
2024-11-01 12:25:54 +0200

    feat: fetch advanced

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
 'use strict'
 
-async function allProducts () {
-        const response = await fetch('https://dummyjson.com/products');
-        const { products } = await response.json();
-        return products
-}
-
-async function getProductById(id) {
-    const response = await fetch(`https://dummyjson.com/products/${id}`);
-    const product = await response.json();
-    return product;
-}
-async function getProductByIdWithError(id) {
-    const response = await fetch(`https://dummyjson.com/productss/${id}`);
-    const product = await response.json();
-    return product;
-}
-
 async function main() {
-    const products = await allProducts();
-
-    const promises = products.map(({id}) => getProductById(id));
-
-    // const productsWithDetails = await Promise.all([...promises, getProductByIdWithError()]);
-    // console.log(productsWithDetails);
-
-    const productsWithDetails2 = await Promise.allSettled([...promises, getProductByIdWithError()]);
-    // console.log(productsWithDetails2.map(({status, value}) => ({status, value})));
-    const productsWithDetails3 = await Promise.race([getProductByIdWithError(), ...promises, allProducts()]);
-    console.log(productsWithDetails3);
+    const response = fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: 'emilys',
+            password: 'emilyspass',
+        }),
+    })
+    const data = await response.json()
+    console.log(data)
 }
-
 main()
