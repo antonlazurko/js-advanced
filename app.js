d8fdb987d6374fd47b03133cd31b6c65c038c883
Anton Lazurko <antla@ciklum.com>
2024-11-01 15:32:05 +0200

    feat: dom events life cycle

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 'use strict'
 
-const wrapper = document.querySelector('.wrapper')
-const inner = document.querySelector('.inner')
-const btn = document.querySelector('.button')
+document.addEventListener('DOMContentLoaded', function(e) {
+    console.log('content loaded', e);
+    console.log(this);
+})
 
-console.log(inner.children);
-
-console.log(inner.parentElement);
-
-console.log(btn.closest('.wrapper'));
-
-console.log(btn.previousElementSibling);
-console.log(btn.nextElementSibling);
-
-console.log(btn.parentElement.children[btn.parentElement.children.length - 1]);
-
-
-
-
+window.addEventListener('load', function (e) {
+    console.log(e);
+    console.log(this);
+})
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault()
+})
