5543aa450cd325c7d1cee3b29bc7f8d0d67e4507
Anton Lazurko <antla@ciklum.com>
2024-11-01 12:09:17 +0200

    feat: parallel execution promise all

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 'use strict'
 
-const arrowAsync = async () => {
-    try {
+async function allProducts () {
         const response = await fetch('https://dummyjson.com/products');
         const { products } = await response.json();
         return products
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
 }
 
-// console.log('1');
+async function getProductById(id) {
+    const response = await fetch(`https://dummyjson.com/products/${id}`);
+    const product = await response.json();
+    return product;
+}
 
-// arrowAsync().then(products => console.log(products)).catch(error => console.log(error));
+async function main() {
+    const products = await allProducts();
+    console.log(products);
+    // for (const {id} of products) {
+    //     const product = await getProductById(id);
+    //     console.log(product);
+    // }
+    const promises = products.map(({id}) => getProductById(id));
+    const productsWithDetails = await Promise.all(promises);
+    console.log(productsWithDetails);
+}
 
-// console.log('2');
-
-(async () => {
-    console.log('1');
-    try {
-
-        const products = await arrowAsync();
-        console.log(products);
-    } catch (error) {
-        console.log(error);
-
-    }
-    console.log('2');
-
-})();
+main()
