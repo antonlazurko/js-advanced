ee0d99c54e7da944bc1ecca5a656c50bd7c5bb12
Anton Lazurko <antla@ciklum.com>
2024-11-01 13:55:33 +0200

    feat: work with elements

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 'use strict'
 
-const url = 'https://dummyjson.com/products/'
+console.log(document);
+const element = document.querySelector('#app');
+const elements = document.querySelectorAll('meta');
+console.log(element);
+console.log(elements);
 
-async function getProductsById() {
-    const response = await fetch(url + Math.floor(Math.random() * 100));
-    return await response.json();
-}
+element.classList.add('active');
+element.textContent = 'Hello World';
 
-async function generate() {
-    try {
-        const wrapper = document.querySelector('.wrapper');
-        const data = await Promise.all([
-            getProductsById(),
-            getProductsById(),
-            getProductsById(),
-            getProductsById(),
-        ]);
-        console.log(data);
-        for(const product of data) {
-            createProductElement(product, wrapper);
-        }
-    } catch (error) {
+const subElement = document.createElement('h1');
+const subElement2 = document.createElement('h1');
+const subElement3 = document.createElement('h1');
+subElement.textContent = 'Hello World';
+subElement2.textContent = 'Hello';
+subElement3.textContent = 'World';
+element.append(subElement);
+element.prepend(subElement2);
+element.after(subElement3);
 
-    }
-}
-function createProductElement(product, wrapper) {
-    const productDiv = document.createElement('div');
+function generate(event) {
 
-    productDiv.classList.add('product');
-    productDiv.innerHTML = `<span>${product.title}</span>`;
-    wrapper.appendChild(productDiv);
+    console.log(event.target.getBoundingClientRect());
+
+    console.log('X offset: ', window.scrollX);
+    console.log('Y offset: ', window.scrollY);
+    console.log('client width: ', document.documentElement.clientWidth);
+    console.log('client height: ', document.documentElement.clientHeight);
+
+    const downEl = document.querySelector('.down');
+    const rect = downEl.getBoundingClientRect();
+
+    window.scrollTo({
+        left: window.scrollX + rect.left,
+        top: window.scrollY + rect.top,
+        behavior: 'smooth'
+    })
+
 }
