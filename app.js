5e2431edc8618ac14448d2fe1775d8cd8a83b913
Anton Lazurko <antla@ciklum.com>
2024-11-01 15:24:29 +0200

    feat: DOM navigation

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 'use strict'
 
-const btn = document.querySelector('button')
 const wrapper = document.querySelector('.wrapper')
 const inner = document.querySelector('.inner')
-for (let i = 0; i < 100; i++) {
-    const div = document.createElement('div')
-    div.classList.add('box')
-    div.textContent = i
-    div.setAttribute('data-id',i)
-    // div.addEventListener('click',function (params) {
-    //     this.remove()
-    // })
-    wrapper.appendChild(div)
-}
-wrapper.addEventListener('click',function (event) {
-    console.log(event.target.dataset.id + ' was deleted');
-    if (event.target.classList.contains('box')) {
-        event.target.remove()
-    }
-})
+const btn = document.querySelector('.button')
+
+console.log(inner.children);
+
+console.log(inner.parentElement);
+
+console.log(btn.closest('.wrapper'));
+
+console.log(btn.previousElementSibling);
+console.log(btn.nextElementSibling);
+
+console.log(btn.parentElement.children[btn.parentElement.children.length - 1]);
+
+
+
+
