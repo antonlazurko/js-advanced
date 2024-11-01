caf3f6dbb41018fed739c2e3cf35643922a43e71
Anton Lazurko <antla@ciklum.com>
2024-11-01 15:48:39 +0200

    feat: exercise list search

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', function(e) {
-    console.log('content loaded', e);
-    console.log(this);
-})
+const input = document.querySelector('input')
+const inner = document.querySelector('.inner')
 
-window.addEventListener('load', function (e) {
-    console.log(e);
-    console.log(this);
-})
-window.addEventListener('beforeunload', function (e) {
-    e.preventDefault()
-})
+for (let i = 0; i < 30; i++) {
+    const box = document.createElement('div')
+    box.textContent = i + 'test text'
+
+    box.classList.add('box')
+    inner.append(box)
+}
+
+function search(e) {
+    const searchValue = e.target.value
+    const boxes = [...inner.children]
+
+    boxes.forEach((box) => {
+        if (!searchValue) {
+            box.classList.remove('active')
+            return
+        }
+        if (box.textContent.includes(searchValue)) {
+            box.classList.add('active')
+            return
+        }
+        box.classList.remove('active')
+    })
+}
+
+input.addEventListener('input', search)
