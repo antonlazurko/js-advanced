783a07a6e06e2efe8de5ef273fb5b07c5f8e4c84
Anton Lazurko <antla@ciklum.com>
2024-11-01 11:57:50 +0200

    feat: execution sequence

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 'use strict'
 
-class ProductREpository {
-    async getProducts() {
+const arrowAsync = async () => {
+    try {
         const response = await fetch('https://dummyjson.com/products');
         const { products } = await response.json();
-        console.log(products);
-
+        return products
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
 
-const repo = new ProductREpository();
-repo.getProducts();
+// console.log('1');
 
-const arrowAsync = async () => {
-    const response = await fetch('https://dummyjson.com/products');
-    const { products } = await response.json();
-    console.log(products);
-}
+// arrowAsync().then(products => console.log(products)).catch(error => console.log(error));
 
-arrowAsync();
+// console.log('2');
+
+(async () => {
+    console.log('1');
+    try {
+
+        const products = await arrowAsync();
+        console.log(products);
+    } catch (error) {
+        console.log(error);
+
+    }
+    console.log('2');
+
+})();
