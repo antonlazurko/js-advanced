8cba37e5731fb702756fa96a107134dffc09f383
Anton Lazurko <antla@ciklum.com>
2024-11-01 11:50:25 +0200

    feat: async methods

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
 'use strict'
 
-const getApiUrl = (latitude, longitude) => `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`;
+class ProductREpository {
+    async getProducts() {
+        const response = await fetch('https://dummyjson.com/products');
+        const { products } = await response.json();
+        console.log(products);
 
-async function getCoordinates() {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords;
-                resolve({latitude, longitude});
-            },
-            (error) => {
-                reject(error);
-            })
-    })
-}
-
-async function getMyCity() {
-    try {
-        const { latitude, longitude } = await getCoordinates();
-
-        const response = await fetch(getApiUrl(latitude, longitude));
-
-        if (response.status !== 200) {
-            throw new Error('Products Error: ', productsResponse.status);
-        }
-        const { city } = await response.json();
-        console.log(city);
-
-    } catch (error) {
-        console.error(error);
-    } finally {
-        console.log('finally');
     }
 }
 
-getMyCity()
+const repo = new ProductREpository();
+repo.getProducts();
+
+const arrowAsync = async () => {
+    const response = await fetch('https://dummyjson.com/products');
+    const { products } = await response.json();
+    console.log(products);
+}
+
+arrowAsync();
